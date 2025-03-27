b6001daec0dc2ade49f082f6b16c383c8e16673f
Nadya <rozhina.nad@gmail.com>
2025-03-27 18:18:21 +0100

    Initial Commit

---
+++ StarWarsExplorer/StarWarsExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsExplorerApp.swift
+//  StarWarsExplorer
+//
+//  Created by Rozhina,Nadya on 2025-03-27.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StarWarsExplorerTests/StarWarsExplorerTests.swift
@@ -0,0 +1,16 @@
+//
+//  StarWarsExplorerTests.swift
+//  StarWarsExplorerTests
+//
+//  Created by Rozhina,Nadya on 2025-03-27.
+//
+
+import Testing
+
+struct StarWarsExplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
